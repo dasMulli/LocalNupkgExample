7827e3ede020cac4242fde0442093c84583a9eb7
dasMulli <martin.andreas.ullrich@gmail.com>
2017-06-10 08:39:13 +0200

    added example runner

---
+++ run-example.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+SOURCE="${BASH_SOURCE[0]}"
+SRC_DIR="$(cd -P "$(dirname "$SOURCE")"&&pwd)"
+
+dotnet nuget locals all --clear
+
+cd "${SRC_DIR}/ExampleLocalLibrary"
+dotnet restore
+dotnet pack -c Release -o ../local-packages
+
+cd "${SRC_DIR}/ExampleApp"
+dotnet add package ExampleLocalLibrary
+dotnet restore
+dotnet run

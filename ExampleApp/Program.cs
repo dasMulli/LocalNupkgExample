a7139213b3c44edeac26d2eef17b12e5ce1ef7d1
dasMulli <martin.andreas.ullrich@gmail.com>
2017-06-10 08:34:16 +0200

    Inital version before adding package

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Value from lib:" + ExampleLocalLibrary.ExampleValues.ExampleValue);
+        }
+    }
+}
---
+++ ExampleLocalLibrary/ExampleValues.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExampleLocalLibrary
+{
+    public static class ExampleValues
+    {
+        public static string ExampleValue => "this is an example value";
+    }
+}
